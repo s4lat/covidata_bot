bfbddb7ca64890256f0f982c2284c6a1cc41e9c9
s4lat <zakazchikm@gmail.com>
2020-04-09 00:35:53 +0500

    Ready to deploy

---
+++ bot.py
@@ -0,0 +1,71 @@
+from telegram.error import (TelegramError, Unauthorized, BadRequest, 
+                            TimedOut, ChatMigrated, NetworkError)
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, JobQueue
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from update_stats import update_pages
+import logging, os
+
+with open('secret.token', 'r') as f:
+	TOKEN = f.read().split()[1]
+
+logging.basicConfig(filename='log.log',level=logging.INFO)
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+jobQ = JobQueue()
+jobQ.set_dispatcher(dispatcher)
+jobQ.run_repeating(update_pages, interval=54000, first=0)
+jobQ.start()
+
+def start(update, context):
+	with open('./pages/out1.jpg', 'rb') as img:
+			context.bot.send_photo(chat_id=update.effective_chat.id,
+			photo=img)
+	
+	num_of_images = len(os.listdir('./pages'))
+
+	keyboard = [['/page 1', '/page 2'],
+						['/page 1', '/page %s' % num_of_images]]
+	reply_markup = ReplyKeyboardMarkup(keyboard)
+
+	return context.bot.send_message(chat_id=update.effective_chat.id, 
+		text="Use keyboard below", 
+		reply_markup=reply_markup)
+
+def show_data(update, context):
+	msg = update.message.text.split()
+	try:
+		n = int(msg[1])
+
+	except ValueError:
+		return context.bot.send_message(chat_id=update.effective_chat.id,
+			text="[ERROR] Please use buttons, not manual commands [ValueError]")
+
+	try:
+		num_of_images = len(os.listdir('./pages'))
+		n = 1 if n <= 0 else n
+		n = num_of_images if n > num_of_images else n
+
+		with open('./pages/out%s.jpg' % n, 'rb') as img:
+			keyboard = [['/page %s' % (n-1), '/page %s' % (n+1)],
+						['/page 1', '/page %s' % num_of_images]]
+
+			reply_markup = ReplyKeyboardMarkup(keyboard)
+
+			return context.bot.send_photo(chat_id=update.effective_chat.id,
+			reply_markup=reply_markup,
+			photo=img)
+
+	except FileNotFoundError:
+			return context.bot.send_message(chat_id=update.effective_chat.id,
+				text="[ERROR] Please use buttons, not manual commands [FileNotFoundError]")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+list_handler = CommandHandler('page', show_data)
+dispatcher.add_handler(list_handler)
+
+updater.start_polling()
+updater.idle()
---
+++ update_stats.py
@@ -0,0 +1,96 @@
+import requests as req
+import json, imgkit, os
+
+with open('page.html') as f:
+		template = f.read()
+
+template = template.replace("#RESETCSS", os.path.abspath('./static/reset.css'))
+template = template.replace("#BOOTSTRAP", os.path.abspath('./static/bootstrap_4.0.0/css/bootstrap.min.css'))
+
+with open('secret.token') as f:
+		TOKEN = f.read().split()[0]
+
+stats_url = "https://covid-193.p.rapidapi.com/statistics"
+countries_urls = "https://covid-193.p.rapidapi.com/countries"
+
+headers = {
+		'x-rapidapi-host': "covid-193.p.rapidapi.com",
+		'x-rapidapi-key': TOKEN
+		}
+
+def get_data():
+	resp = req.request("GET", stats_url, headers=headers)
+	data = json.loads(resp.text)
+
+	while data['errors']:
+		resp = req.request("GET", stats_url, headers=headers)
+		data = json.loads(resp.text)
+
+	data['response'] = [d for d in data['response'] if d['country'] != 'All']
+
+	return data['response']
+
+def update_pages(*args):
+	data = get_data()
+
+	data = sorted(data, key=lambda k: k['cases']['active'], reverse=True)
+	
+	pages = []
+	c = 1
+	html = template
+	for i, d in enumerate(data):
+		html = html.replace('#IND', str(i), 1)
+		html = html.replace('#Country', str(d['country']), 1)
+		html = html.replace('#Active', str(d['cases']['active']), 1)
+
+		if d['cases']['new']:
+			if int(d['cases']['new']) <= 0:
+				html = html.replace('#NACOLOR', 'rgb(25, 200, 0)', 1)
+				html = html.replace('#NA', d['cases']['new'], 1)
+			else:
+				html = html.replace('#NACOLOR', 'rgb(255, 25, 0)', 1)
+				html = html.replace('#NA', d['cases']['new'], 1)
+		else:
+			html = html.replace('#NACOLOR', '', 1)
+			html = html.replace('[#NA]', '', 1)
+
+
+		html = html.replace('#Recovered', str(d['cases']['recovered']), 1)
+		html = html.replace('#Deaths', str(d['deaths']['total']), 1)
+
+		if  d['deaths']['new']:
+			if int(d['deaths']['new']) <= 0:
+				html = html.replace('#NDCOLOR', 'rgb(25, 200, 0)', 1)
+				html = html.replace('#ND', d['deaths']['new'], 1)
+			else:
+				html = html.replace('#NDCOLOR', 'rgb(255, 25, 0)', 1)
+				html = html.replace('#ND', d['deaths']['new'], 1)
+		else:
+			html = html.replace('#NDCOLOR', '', 1)
+			html = html.replace('[#ND]', '', 1)
+
+
+		html = html.replace('#Total', str(d['cases']['total']), 1)
+		if (i+1) % 10 == 0:
+			imgkit.from_string(html, 'pages/out%s.jpg' % c)
+			html = template
+			c += 1
+
+	if html != template:
+		html = html.replace('#Country', '')
+		html = html.replace('#Active', '')
+		html = html.replace('#Recovered', '')
+		html = html.replace('#Deaths', '')
+		html = html.replace('#Total', '')
+		html = html.replace('#IND', '')
+		html = html.replace('[#ND]', '')
+		html = html.replace('[#NA]', '')
+
+		imgkit.from_string(html, 'pages/out%s.jpg' % c)
+
+	return pages
+
+
+
+
+
